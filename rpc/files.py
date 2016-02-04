6d6c0eee76775c4ae573b45e1ebb79c00f6a39db
Yuki Suga <ysuga@ysuga.net>
2016-02-04 12:44:55 +0900

    files api test passed.

--- rpc/files.py
+++ rpc/files.py
@@ -7,49 +7,72 @@ class FilesPlugin(PluginObject):
     def __init__(self):
         PluginObject.__init__(self, 'files')
 
+        
     def list_directory(self, path):
-        sys.stdout.write('files_list_directory(%s)\n' % path)
+        """ List directory 
+        :param string path: Path for listing
+        :rtype: tupple
+        :return: tupple(Successflag, Message, List of directory)
+        """
+        self.debug('files_list_directory(%s)' % path)
         try:
-            return [True, os.listdir(path)]
-        except:
+            return self.return_value(True, '', os.listdir(path))
+        except Exception, ex:
             traceback.print_exc()
-        return [False, []]
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
 
     def change_directory(self, path):
+        """ Change directory
+        :param string path: Destination path.
+        :rtype: tupple
+        :return: tupple(Success flag, Message, current directory after changed.)
+        """
+        self.debug('change_directory(%s)' % path)
         try:
             os.chdir(path)
-            return [True, os.getcwd()]
-        except:
+            return self.return_value(True, '', os.getcwd())
+        except Exception, ex:
             traceback.print_exc()
-        return [False, '']
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
       
     def print_working_directory(self):
+        """ Print Current Directory
+        """
+        self.debug('print_working_directory')
         try:
-            return [True, os.getcwd()]
-        except:
+            return self.return_value(True, '', os.getcwd())
+        except Exception, ex:
             traceback.print_exc()
-        return [False, '']
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
 
     def upload_file(self, filename, file_content):
+        self.debug('upload_file(%s, %s)' % (filename, file_content))
         try:
             open(filename, 'w').write(file_content)
-            return [True, filename]
-        except:
+            return self.return_value(True, '', filename)
+        except Exception, ex:
             traceback.print_exc()
-        return [False, '']
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
 
     def download_file(self, filename):
+        self.debug('download_file(%s)' % filename)
         try:
-            return [True, open(filename, 'r').read()]
-        except:
+            return (True, '', open(filename, 'r').read())
+        except Exception, ex:
             traceback.print_exc()
-        return [False, '']
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
 
     def delete_file(self, filename):
+        self.debug('delete_file(%s)' % filename)
         try:
-            sys.stdout.write('delete_file %s\n' % filename)
             os.remove(filename)
-            return [True, '']
-        except:
+            return (True, '', filename)
+        except Exception, ex:
             traceback.print_exc()
-        return [False, '']
+            return self.return_value(False, 'Exception: %s' % str(ex), [])
+        pass
--- rpc/plugin.py
+++ rpc/plugin.py
@@ -1,3 +1,4 @@
+import sys
 
 class PluginObject:
     def __init__(self, name):
@@ -6,3 +7,11 @@ class PluginObject:
     @property
     def name(self):
         return self._name
+
+    def debug(self, msg):
+        sys.stdout.write('Plugin(%s) %s\n' % (self._name, str(msg)))
+        pass
+
+    def return_value(self, success_flag, msg, return_value):
+        self.debug('return_value is (%s, %s, %s)' % (success_flag, msg, return_value))
+        return (success_flag, msg, return_value)
